e087111430b9044f8d78071ae0d63a20052a47f5
Afroz Sheikh <147607954+AfrozSheikh@users.noreply.github.com>
2023-12-19 15:55:47 +0000

    calculated

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-const display = document.querySelector(".result") ; 
+let inputDisplay = document.querySelector(".display-input") 
+
+let resultDisplay = document.querySelector(".display-result")
+let string= "" ;
+let buttons = document.querySelectorAll(".btn") ; 
+
+Array.from(buttons).forEach((button)=>{
+    button.addEventListener('click',(e)=>{
+        if(e.target.innerHTML == "=") {
+            string= eval(string) ; 
+            inputDisplay.value= string;
+        }
+        else if(e.target.innerHTML == "AC") {
+            string="" ;
+            inputDisplay.value= string;
+
+        }
+        else if (e.target.innerHTML == "r"){
+                 string =string.substring(0,string.length-1) ;
+                 inputDisplay.value= string;
+        }
+        else{
+        string = string + e.target.innerHTML ; 
+        inputDisplay.value= string;}
+    }) 
+
+})
+ 
+ 
+
+
